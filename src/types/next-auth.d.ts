8a84e22d1a0325bc615d5136559530d9e76341ee
Mukul Kashyap <mukulkashyap.business@gmail.com>
2026-03-18 17:01:24 +0530

    Add a cleaner implementation of module augmentation

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -5,10 +5,10 @@ declare module "next-auth" {
   interface Session {
     user: {
       id?: string;
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
-    };
+      // name?: string | null;
+      // email?: string | null;
+      // image?: string | null;
+    } & DefaultSession["user"];
   }
 }
 
